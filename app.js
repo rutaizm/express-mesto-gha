8c6bb4f57d26785d8195489de586c832681a39e3
Tatyana Izmailova <rutaizm@yandex.ru>
2022-08-11 13:55:56 +0300

    added celebrate in app

--- app.js
+++ app.js
@@ -1,11 +1,14 @@
 const express = require('express');
 const bodyParser = require('body-parser');
 const mongoose = require('mongoose');
+const { errors } = require('celebrate');
 const { NOT_FOUND } = require('./utils/errors');
 
 const app = express();
 const routesUsers = require('./routes/users');
 const routesCard = require('./routes/cards');
+
+const { validationSignUp, validationSignIn } = require('./middlewares/validation');
 
 const auth = require('./middlewares/auth');
 
@@ -17,8 +20,8 @@ app.use(bodyParser.urlencoded({ extended: true }));
 
 const { login, createUser } = require('./controllers/users');
 
-app.post('/signin', login);
-app.post('/signup', createUser);
+app.post('/signin', validationSignIn, login);
+app.post('/signup', validationSignUp, createUser);
 
 app.use(auth);
 
@@ -28,5 +31,7 @@ app.use('/cards', routesCard);
 app.use((req, res) => {
   res.status(NOT_FOUND).send({ message: 'Страница не найдена' });
 });
+
+app.use(errors());
 
 app.listen(PORT);
